94cba6dc497737def1c60e8d06031ebd6c6aff48
arij <hajlaoui.arij@gmail.com>
2020-11-01 20:52:11 +0100

    sign in sign up

--- src/main/java/mycompany/base.java
+++ src/main/java/mycompany/base.java
@@ -4,6 +4,10 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 
 public class base {
@@ -17,5 +21,8 @@ public class base {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+	}@AfterMethod
+	public void close() {
+		driver.quit();
 	}
 }
